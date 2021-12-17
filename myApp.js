2b078ab08f26be26f5e9add33f5f71cb8d76ce39
mbanani-git <leedbn1@gmail.com>
2021-12-18 00:20:59 +0100

    kk

--- myApp.js
+++ myApp.js
@@ -2,21 +2,27 @@ var express = require("express");
 var app = express();
 const dotenv = require("dotenv");
 dotenv.config();
-dotenv.load();
+
 app.get("/", (req, res) => {
   const absolutePath = __dirname + "/views/index.html";
   res.sendFile(absolutePath);
 });
 
-app.get("/json", (req, res) => {
-  let response;
-  if (process.env.MESSAGE_STYLE == "uppercase") {
-    response = "Hello json".toUpperCase();
-  } else {
-    response = "Hello json"; //ok
-  }
-  res.json({ message: response });
+// app.get("/json", (req, res) => {
+//   let response;
+//   if (process.env.MESSAGE_STYLE == "uppercase") {
+//     response = "Hello json".toUpperCase();
+//   } else {
+//     response = "Hello json"; //ok
+//   }
+//   res.json({ message: response });
+// });
+app.get("/json", (req, res, next) => {
+  let methode = req.method;
+  let path = req.path;
+  let ip = req.ip;
+  res.json({ methode: methode + " " + path + " - " + ip });
+  next();
 });
-
 app.use("/public", express.static(__dirname + "/public"));
 module.exports = app;
