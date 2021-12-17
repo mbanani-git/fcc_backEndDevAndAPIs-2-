33108be490c9e4b86c3a3cec122eac8db05d0c35
mbanani-git <leedbn1@gmail.com>
2021-12-17 01:11:19 +0100

    ok

--- myApp.js
+++ myApp.js
@@ -2,7 +2,8 @@ var express = require("express");
 var app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  const absolutePath = __dirname + "/views/index.html";
+  res.sendFile(absolutePath);
 });
 
 module.exports = app;
