494e755bb0615bd9443e6ee2bf11d21791e82b13
mbanani-git <leedbn1@gmail.com>
2021-12-18 00:33:11 +0100

    ok

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 const dotenv = require("dotenv");
 dotenv.config();
-
+let router = express.Router();
 app.get("/", (req, res) => {
   const absolutePath = __dirname + "/views/index.html";
   res.sendFile(absolutePath);
@@ -17,12 +17,18 @@ app.get("/", (req, res) => {
 //   }
 //   res.json({ message: response });
 // });
-app.get("/json", (req, res, next) => {
-  let methode = req.method;
-  let path = req.path;
-  let ip = req.ip;
-  res.json({ methode: methode + " " + path + " - " + ip });
+// app.get("/json", (req, res, next) => {
+//   let methode = req.method;
+//   let path = req.path;
+//   let ip = req.ip;
+//   res.json({ methode: methode + " " + path + " - " + ip });
+//   next();
+// });
+
+app.use("/public", express.static(__dirname + "/public"));
+app.use(function (req, res, next) {
+  let string = req.method + " " + req.path + " - " + req.ip;
+  console.log(string);
   next();
 });
-app.use("/public", express.static(__dirname + "/public"));
 module.exports = app;
